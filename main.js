1ba16c5d712a1d3f4439a6e92205e32b08e5db04
Jaerl0 <ajarlo03@gmail.com>
2021-02-05 15:27:53 +0100

    Fixade på drawing.html och i javascript där jag gjorde funktioner för att kunna rita samt kunna ändra på färgen

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
-let user = {
-    name: "Goran",
-    admin: true,
-    loggedIn: false,
-    friends: ["Fredrik", "Filippa"],
-};
 
-console.table(user);
+
+window.addEventListener("load", () => {
+    const drawingArea = document.querySelector("#drawingArea");
+    const ctx = drawingArea.getContext("2d");
+
+    drawingArea.height = window.innerWidth/1.5;
+    drawingArea.width = window.innerWidth;
+
+   let painting = false;
+   let strokeSize = 8;
+
+// Change strokecolor
+   function changeStrokeColor (){
+       var color = document.getElementById("colorSelect").value;
+       ctx.strokeStyle = color;
+   }
+   document.getElementById("apply").addEventListener("click", changeStrokeColor, false);
+
+   function startPosition(e){
+       painting = true;
+       draw(e);
+   }
+   function finishedPosition() {
+       painting = false;
+       ctx.beginPath();
+   }
+   function draw(e){
+       if(!painting) return;
+       ctx.lineWidth = strokeSize;
+       ctx.lineCap = "round";
+
+       ctx.lineTo(e.clientX, e.clientY-100);
+       ctx.stroke();
+       ctx.beginPath();
+       ctx.moveTo(e.clientX, e.clientY-100);
+   }
+
+   drawingArea.addEventListener("mousedown", startPosition);
+   drawingArea.addEventListener("mouseup", finishedPosition);
+   drawingArea.addEventListener("mousemove", draw);
+});
