458131dde69fc1e67b149e84eb5de8ba179f33a7
Jaerl0 <ajarlo03@gmail.com>
2021-02-10 15:15:11 +0100

    Fixade med javascript för min drawing.html samt styling för den

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ window.addEventListener("load", () => {
     const drawingArea = document.querySelector("#drawingArea");
     const ctx = drawingArea.getContext("2d");
 
-    drawingArea.height = window.innerWidth/1.5;
+    drawingArea.height = 800;
     drawingArea.width = window.innerWidth;
 
    let painting = false;
-   let strokeSize = 8;
 
 // Change strokecolor
    function changeStrokeColor (){
@@ -16,6 +15,28 @@ window.addEventListener("load", () => {
        ctx.strokeStyle = color;
    }
    document.getElementById("apply").addEventListener("click", changeStrokeColor, false);
+
+// Change strokesize
+   function changeStrokeSize (){
+       var StrokeSize = document.getElementById("size").value;
+       ctx.lineWidth = StrokeSize;
+   }
+   document.getElementById("apply").addEventListener("click", changeStrokeSize, false);
+
+// Clear canvas 
+document.getElementById("clear").addEventListener("click", function() {
+    ctx.clearRect(0, 0, drawingArea.width, drawingArea.height); }, false);
+
+// Eraser 
+
+    function eraser (){
+        var eraser = document.getElementById("eraser").value;
+        if (eraser == true){
+        ctx.strokeStyle = "white";
+        }
+    }
+    document.getElementById("eraser").addEventListener("click", eraser, false);
+
 
    function startPosition(e){
        painting = true;
@@ -27,7 +48,6 @@ window.addEventListener("load", () => {
    }
    function draw(e){
        if(!painting) return;
-       ctx.lineWidth = strokeSize;
        ctx.lineCap = "round";
 
        ctx.lineTo(e.clientX, e.clientY-100);
